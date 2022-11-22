a6bdbf8b7ecb0ba9af5f446777ecb5af71195afe
Nikolay Pomytkin <nikolayp2800@gmail.com>
2022-11-21 19:47:29 -0600

    Finished initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Vite + React</h1>
+      <div className='card'></div>
+    </div>
+  )
+}
+
+export default App
